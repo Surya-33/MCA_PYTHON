000dfc7afcbb9e9e85482ddb9946cafaba32661a
Surya-33 <suryakrishnafeb132004@gmail.com>
2024-10-04 11:34:37 +0530

    Add files via upload

---
+++ positinegatizero.py
@@ -0,0 +1,85 @@
+0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+value=int(input("enter a number"))
+print(value)
+if value>=0:
+    if value==0:
+        print("value is zero")
+    else:
+        print("value is positive number")
+else:
+    print("value is negative number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a=[0]
